32e733b2649643cad6dbe0d1ae088932fff79d8c
Julien Gilli <jgilli@fastmail.fm>
2014-06-25 15:06:03 -0400

    Fix indentation in some files. Also convert tabs to spaces in these files.

--- debug.c
+++ debug.c
@@ -8,56 +8,56 @@
 
 struct debug_context_s
 {
-	const char* name;
-	int enabled;
+    const char* name;
+    int         enabled;
 };
 
 DEBUG_API debug_context_t* debug_create_context(const char* name)
 {
-	debug_context_t* new_debug_context = NULL;
-	const char* debug_env              = NULL;
+    debug_context_t* new_debug_context = NULL;
+    const char* debug_env              = NULL;
 
-	new_debug_context = malloc(sizeof(debug_context_t));
-	if (!new_debug_context)
-		return NULL;
+    new_debug_context = malloc(sizeof(debug_context_t));
+    if (!new_debug_context)
+        return NULL;
 
-	if (name)
-		new_debug_context->name = strdup(name);
-	else
-		new_debug_context->name = NULL;
+    if (name)
+        new_debug_context->name = strdup(name);
+    else
+        new_debug_context->name = NULL;
 
-	new_debug_context->enabled = 0;
-	debug_env = getenv("DEBUG");
-	if (debug_env && !strcmp(name, debug_env))
-	{
-		new_debug_context->enabled = 1;
-	}
+    new_debug_context->enabled = 0;
+    debug_env = getenv("DEBUG");
+    if (debug_env && !strcmp(name, debug_env))
+    {
+        new_debug_context->enabled = 1;
+    }
 
-	return new_debug_context;
+    return new_debug_context;
 }
 
 DEBUG_API void debug(const debug_context_t* context,
-					 const char* format,
-					 ...)
+                     const char* format,
+                     ...)
 {
-	va_list argp;
+    va_list argp;
 
-	va_start(argp, format);
-	vdebug(context, format, argp);
-	va_end(argp);
+    va_start(argp, format);
+    vdebug(context, format, argp);
+    va_end(argp);
 }
 
 DEBUG_API void vdebug(const debug_context_t* context, const char* format, va_list ap)
 {
-	assert(context);
-	assert(format);
+    assert(context);
+    assert(format);
 
-	if (!context || !format)
-		return;
+    if (!context || !format)
+        return;
 
-	if (context && !context->enabled)
-		return;
+    if (context && !context->enabled)
+        return;
 
-	fprintf(stderr, "[%s] ", context->name);
-	vfprintf(stderr, format, ap);
+    fprintf(stderr, "[%s] ", context->name);
+    vfprintf(stderr, format, ap);
 }
--- debug.h
+++ debug.h
@@ -6,15 +6,15 @@ extern "C" {
 #endif /* __cplusplus */
 
 #ifdef _WIN32
-	#ifdef DEBUG_EXPORTING
-		#define DEBUG_API __declspec(dllexport)
-	#elif !defined(DEBUG_STATIC)
-		#define DEBUG_API __declscpec(dllimport)
-	#else
-		#define DEBUG_API
-	#endif /* DEBUG_EXPORTING */
+    #ifdef DEBUG_EXPORTING
+        #define DEBUG_API __declspec(dllexport)
+    #elif !defined(DEBUG_STATIC)
+        #define DEBUG_API __declscpec(dllimport)
+    #else
+        #define DEBUG_API
+    #endif /* DEBUG_EXPORTING */
 #else
-	#define DEBUG_API
+    #define DEBUG_API
 #endif /* _WIN32 */
 
 typedef struct debug_context_s debug_context_t;
@@ -23,25 +23,25 @@ DEBUG_API debug_context_t* debug_create_context(const char* name);
 DEBUG_API void debug(const debug_context_t* context, const char* format, ...);
 DEBUG_API void vdebug(const debug_context_t* context, const char* format, va_list ap);
 
-#define CREATE_DEBUG_CTX(ctx_name) \
-static debug_context_t* ctx_name ##_debug_ctx = NULL; \
- \
-static void ctx_name ## _create_dbg_ctx(void) \
-{ \
-	if (!ctx_name ## _debug_ctx) \
-	{ \
-		ctx_name ## _debug_ctx = debug_create_context(#ctx_name); \
-	} \
-} \
- \
-static void ctx_name ## _debug(const char* format, ...) \
-{ \
-	va_list argp; \
- \
-	ctx_name ## _create_dbg_ctx(); \
- \
-	va_start(argp, format); \
-	vdebug(ctx_name ## _debug_ctx, format, argp); \
+#define CREATE_DEBUG_CTX(ctx_name)                                  \
+static debug_context_t* ctx_name ##_debug_ctx = NULL;               \
+                                                                    \
+static void ctx_name ## _create_dbg_ctx(void)                       \
+{                                                                   \
+    if (!ctx_name ## _debug_ctx)                                    \
+    {                                                               \
+        ctx_name ## _debug_ctx = debug_create_context(#ctx_name);   \
+    }                                                               \
+}                                                                   \
+                                                                    \
+static void ctx_name ## _debug(const char* format, ...)             \
+{                                                                   \
+    va_list argp;                                                   \
+                                                                    \
+    ctx_name ## _create_dbg_ctx();                                  \
+                                                                    \
+    va_start(argp, format);                                         \
+    vdebug(ctx_name ## _debug_ctx, format, argp);                   \
 }
 
 #ifdef __cplusplus
